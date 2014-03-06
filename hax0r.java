c7e997748ddb10f2822f364c1f5395dd2a9522ca
Mac Kiser <mkiser96@gmail.com>
2014-03-05 19:42:29 -0500

    Update hax0r.java

--- hax0r.java
+++ hax0r.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import javax.swing.JOptionPane;
  
 class hax0r {
   public static void main(String[] args) {
@@ -8,10 +9,22 @@ class hax0r {
     int i = 0;
  
     try {
+    	
     	while (i < 1)
     	{
+    		
+      // opens notepad
       rs.exec("notepad");
+      
+      // opens an dialog box
+      JOptionPane.showMessageDialog(null, "Sar...");
+      
+      // opens command prompt
+      String path = "c:\\";
+      Runtime.getRuntime().exec(new String[] { "cmd.exe", "/C", "\"start; cd "+path+"\"" });
+      
     	}
+    	
     }
     catch (IOException e) {
       System.out.println(e);
